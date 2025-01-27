9f82783684cc2084ebdbded49f985f52dfb1a3e1
Gabriel Carmona <gabriel.carmonat@sansano.usm.cl>
2025-01-27 18:03:01 +0100

    creating file to compress tree

--- k2bp_compr.cpp
+++ k2bp_compr.cpp
@@ -30,28 +30,32 @@ int main(int argc, char** argv) {
     k2tree_bp_sdsl<2> k2tree;
     k2tree.load(k2_file);
 
+    k2_file.close();
+
+    cerr << "Reading k2tree..." << endl;
+
     auto ones = k2tree.get_pos_ones();
 
     cerr << "Building k2tree..." << endl;
-
-
-    cerr << "Checking if it is correct..." << endl;
-
-    auto check = k2tree.get_pos_ones();
-    sort(check.begin(), check.end());
 
     k2tree_bp_sdsl_idems<2,
       sd_vector<>, rank_support_sd<1>,
       sd_vector<>, rank_support_sd<1>, rank_support_sd<0>,
       select_support_sd<1>, select_support_sd<0>> k2tree_idem(k2tree);
+
+    cerr << "Checking if it is correct..." << endl;
+
+    auto check = k2tree_idem.get_pos_ones();
+    sort(check.begin(), check.end());
+    sort(ones.begin(), ones.end());
     assert(check == ones);
 
     cerr << "Writing file..." << endl;
 
     ofstream k2_file_idem;
-    k2_file.open(k2_path + ".k2bpi");
-    k2tree_idem.write(k2_file);
-    k2_file.close();
+    k2_file_idem.open(k2_path + ".k2bpi");
+    k2tree_idem.write(k2_file_idem);
+    k2_file_idem.close();
 
   }
   return 0;
