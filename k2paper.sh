556a56690906ba2d74a8a150e1c8a05ad9d5f596
Gabriel Carmona <gabriel.carmona@phd.unipi.it>
2025-02-17 14:47:50 +0000

    bash code to test examples

---
+++ k2paper.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+echo "INDOCHINA 2004"
+echo "BUILDING"
+#./k2sparse.x /data/matrix/webg/indochina-2004.mtx.rowm -s 7414866
+echo "MULTIPLY"
+/usr/bin/time ./k2bp_mult_1.x /data/matrix/webg/indochina-2004.mtx.rowm.k2bprrr /data/matrix/webg/indochina-2004.mtx.rowm.k2bprrr
+echo "--------------"
+
+echo "ARABIC 2005"
+echo "BUILDING"
+#./k2bp_build.x /data/matrix/webg/arabic-2005.mtx.rowm 22744080 639999458
+echo "MULTIPLY"
+/usr/bin/time ./k2bp_mult_1.x /data/matrix/webg/arabic-2005.mtx.rowm.k2bprrr /data/matrix/webg/arabic-2005.mtx.rowm.k2bprrr
+echo "--------------"
+
+
+echo "UK 2005"
+echo "BUILDING"
+#./k2bp_build.x /data/matrix/webg/uk-2005.mtx.rowm 39459925 936364282
+echo "MULTIPLY"
+/usr/bin/time ./k2bp_mult_1.x /data/matrix/webg/uk-2005.mtx.rowm.k2bprrr /data/matrix/webg/uk-2005.mtx.rowm.k2bprrr
+echo "--------------"
+
