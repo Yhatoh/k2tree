c31bd11dfa68976f247b2aeec058a317a4d0235c
Gabriel Carmona <gabriel.carmonat@sansano.usm.cl>
2026-02-24 10:53:09 +0100

    add info child supp

--- include/k2_bp.hpp
+++ include/k2_bp.hpp
@@ -827,6 +827,7 @@ class k2_bp {
       cout << "  Tree        : " << (size_in_bytes(tree)) * 8 << "," << (double) (size_in_bytes(tree)) * 8 / size() << "," << (double) (size_in_bytes(tree)) * 8 / total << endl;
       cout << "  Tree Support: " << (size_in_bytes(tree_support)) * 8 << "," << (double) (size_in_bytes(tree_support)) * 8 / size() << "," << (double) (size_in_bytes(tree_support)) * 8 / total << endl;
       cout << "  L           : " << (size_in_bytes(l)) * 8 << "," << (double) (size_in_bytes(l)) * 8 / size() << "," << (double) (size_in_bytes(l)) * 8 / total << endl;
+      cout << "  child supp  : " << child_support.size() * sizeof(uint64_t) * 8 << "," << (double) (child_support.size() * sizeof(uint64_t) * 8) / size() << "," << (double) child_support.size() * sizeof(uint64_t) * 8 / total << endl;
       cout << "  leaves      : " << (size_in_bytes(leaves) + size_in_bytes(rank_leaves)) * 8 << "," << (double) (size_in_bytes(leaves) + size_in_bytes(rank_leaves)) * 8 / size() << "," << (double) (size_in_bytes(leaves) + size_in_bytes(rank_leaves)) * 8 / total << endl;
 #endif
       return total;
