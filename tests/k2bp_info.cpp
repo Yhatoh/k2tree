d691c703764cb10a19c5d0b839f92d2d8491b64b
Gabriel Carmona <gabriel.carmonat@sansano.usm.cl>
2026-02-25 14:58:55 +0100

    add options info file

--- tests/k2bp_info.cpp
+++ tests/k2bp_info.cpp
@@ -4,32 +4,53 @@
 // local includes
 #include "k2_bp.hpp"
 
+void usage_and_exit(char* name);
+
 int main(int argc, char** argv) {
-  if(argc <= 1) {
-    std::cerr << "At least one argument:" << endl;
-    std::cerr << "  ./k2bp_info.x <path K2-BP1> ... <path K2-BPn>" << endl;
-    exit(1);
+  extern char *optarg;
+  extern int optind, opterr, optopt;
+
+  int type = 0;
+  int c;
+
+  while((c=getopt(argc, argv, "bt:cs:")) != -1) {
+    switch(c) {
+      case 'b':
+        type = 1; break;
+      case '?':
+        cerr << "Unkown option: " << optarg << std::endl;
+        exit(1);
+    }
   }
 
-  for(uint64_t i = 1; i < argc; i += 1) {
-    std::string k2_path = argv[i];
-
-    cout << k2_path << endl;
-    ifstream k2_file;
-    k2_file.open(k2_path);
-
-    if(!k2_file.is_open()) {
-      cerr << "Error opening file. Check if the file exists or the path is writed correctly" << endl;
-      exit(1);
-    }
-
+  optind -= 1;
+  if(argc - optind != 2) usage_and_exit(argv[0]);
+  argv += optind; argc -= optind;
+  std::string matrix = argv[1];
+  std::ifstream k2_file(matrix);
+  
+  if(type == 0) {
     k2_bp<2, bit_vector> k2tree;
     k2tree.load(k2_file);
 
     k2_file.close();
 
 
-    cout << "Information k2tree " << k2_path << endl;
+    cout << "Information k2tree " << matrix << endl;
+    cout << " Size Matrix: " << k2tree.size_matrix() << " Amount of 1's: " << k2tree.size() << endl;
+    cout << " Amount of nodes: " << k2tree.nodes() << endl;
+    uint64_t bits = k2tree.size_in_bits();
+    cout << " Bits    : " << bits << endl;
+    cout << " Bits/1's: " << (double) bits / k2tree.size() << endl;
+    cout << " Bits/n  : " << (double) bits / k2tree.nodes() << endl;
+  } else { 
+    k2_bp<2, rrr_vector<127>> k2tree;
+    k2tree.load(k2_file);
+
+    k2_file.close();
+
+
+    cout << "Information k2tree " << matrix << endl;
     cout << " Size Matrix: " << k2tree.size_matrix() << " Amount of 1's: " << k2tree.size() << endl;
     cout << " Amount of nodes: " << k2tree.nodes() << endl;
     uint64_t bits = k2tree.size_in_bits();
@@ -37,5 +58,14 @@ int main(int argc, char** argv) {
     cout << " Bits/1's: " << (double) bits / k2tree.size() << endl;
     cout << " Bits/n  : " << (double) bits / k2tree.nodes() << endl;
   }
+
   return 0;
+}
+
+void usage_and_exit(char* name) {
+    cerr << "Usage:\n\t  " << name <<  " [options] filename \n\n";
+    cerr << "Options:\n";
+    cerr << "\t-b        use rrr_vector<127> to store leaves (def. bit_vector) [bit_vector more space, faster; rrr_vector less space, slower]\n";    
+    cerr << "Show info about compress matrix in filename\n\n";
+    exit(1);
 }
