4c9e480645179395452b415c010beefd505e758a
Gabriel Carmona <gabriel.carmonat@sansano.usm.cl>
2026-03-06 10:05:54 +0100

    remove building samples in mul

--- c/k2bp/k2bp.c
+++ c/k2bp/k2bp.c
@@ -845,8 +845,6 @@ static void reck2bp_mul(k2bp_traversal_t* pos_a, const k2bp_t* a, k2bp_traversal
   reck2bp_mul(&(as1[1]), a, &(bs1[2]), b, &(aux_c[1]));
   aux_c[0].threshold = aux_c[0].t.n;
   aux_c[1].threshold = aux_c[1].t.n;
-  k2bp_build_exc_sampling(&(aux_c[0]));
-  k2bp_build_exc_sampling(&(aux_c[1]));
   k2bp_sum(&(aux_c[0]), &(aux_c[1]), &(c_[0]));
   aux_c[0].n_l = 0; aux_c[0].t.n = 0;
   aux_c[1].n_l = 0; aux_c[1].t.n = 0;
@@ -855,8 +853,6 @@ static void reck2bp_mul(k2bp_traversal_t* pos_a, const k2bp_t* a, k2bp_traversal
   reck2bp_mul(&(as2[1]), a, &(bs1[3]), b, &(aux_c[1]));
   aux_c[0].threshold = aux_c[0].t.n;
   aux_c[1].threshold = aux_c[1].t.n;
-  k2bp_build_exc_sampling(&(aux_c[0]));
-  k2bp_build_exc_sampling(&(aux_c[1]));
   k2bp_sum(&(aux_c[0]), &(aux_c[1]), &(c_[1]));
   aux_c[0].n_l = 0; aux_c[0].t.n = 0;
   aux_c[1].n_l = 0; aux_c[1].t.n = 0;
@@ -865,8 +861,6 @@ static void reck2bp_mul(k2bp_traversal_t* pos_a, const k2bp_t* a, k2bp_traversal
   reck2bp_mul(&(as1[3]), a, &(bs2[2]), b, &(aux_c[1]));
   aux_c[0].threshold = aux_c[0].t.n;
   aux_c[1].threshold = aux_c[1].t.n;
-  k2bp_build_exc_sampling(&(aux_c[0]));
-  k2bp_build_exc_sampling(&(aux_c[1]));
   k2bp_sum(&(aux_c[0]), &(aux_c[1]), &(c_[2]));
   aux_c[0].n_l = 0; aux_c[0].t.n = 0;
   aux_c[1].n_l = 0; aux_c[1].t.n = 0;
@@ -875,8 +869,6 @@ static void reck2bp_mul(k2bp_traversal_t* pos_a, const k2bp_t* a, k2bp_traversal
   reck2bp_mul(&(as2[3]), a, &(bs2[3]), b, &(aux_c[1]));
   aux_c[0].threshold = aux_c[0].t.n;
   aux_c[1].threshold = aux_c[1].t.n;
-  k2bp_build_exc_sampling(&(aux_c[0]));
-  k2bp_build_exc_sampling(&(aux_c[1]));
   k2bp_sum(&(aux_c[0]), &(aux_c[1]), &(c_[3]));
 
   k2bp_free(&(aux_c[0]));
