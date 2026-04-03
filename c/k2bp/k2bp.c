c11afa2e402697e3ec39a456da8d3885bdaf3d76
Gabriel Carmona <gabriel.carmonat@sansano.usm.cl>
2026-04-03 16:04:51 +0200

    extra rank eliminated

--- c/k2bp/k2bp.c
+++ c/k2bp/k2bp.c
@@ -1321,7 +1321,7 @@ static void build_leaves_pointers(k2bp_traversal_t* pos_a, const k2bp_t* a) {
       pos_a->leaves_pointers[i] = pos_start.leaves;
     else
       pos_a->leaves_pointers[i] = pos_start.leaves +
-                                  k2bp_leaves_between_pointers(pos_a, rank_p(&pos_start, a), amount_p) +
+                                  k2bp_leaves_between_pointers(pos_a, pos_start.i_p, amount_p) +
                                   pos_a->leaves_pointers[i - 1];
 
     if(a->subtreeinfo != NULL)
